2bac18cca7bd235a09fef2ca2ed47cff00b0170c
alaa gnt <oa_geunat>
2025-02-16 22:29:32 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/Pages/ToDO_page.dart';
+
+
+
+void main() { 
+    runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: TodoPage()
+        
+    );
+        }
+    }
